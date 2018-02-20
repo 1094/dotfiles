eb8aa3b21d3e19f682df5e91039cf1c955bb29ad
1094 <dd3953@gmail.com>
2018-02-20 08:01:31 -0500

    moving scripts to a new repo

--- txt/music/add-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-image=$1
-echo "song? "
-read -r song
-eyeD3 --add-image "$image":FRONT_COVER /home/sir/sounds/mp3/"$song"
--- txt/practice/short-name.sh
+++
@@ -1,8 +0,0 @@
-set -e
-echo
-
-files=(~/sounds/yt/*)
-file=${files[RANDOM % ${#files[@]}]}
-basename "$file" .mp3
-
-echo
